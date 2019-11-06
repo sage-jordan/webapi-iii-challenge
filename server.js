7722c8df395758b782dccfecc98271b63ef2c07a
sage-jordan <sagemjrodan@gmail.com>
2019-11-05 21:34:46 -0500

    server running on 5000 server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
+const postRouter = require('./posts/postRouter');
+
+server.use('/post',  postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -11,5 +14,13 @@ server.get('/', (req, res) => {
 function logger(req, res, next) {
 
 };
+
+server.use('/', () => {
+  res.status(200).send('Express Running');
+})
+
+server.listen(5000, () => {
+  console.log(`=== Server listening on port 5000 ===`);
+}) 
 
 module.exports = server;
