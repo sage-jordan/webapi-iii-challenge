ba13869d864ece5290efa0ddf2a62aaec636dbd2
sage-jordan <sagemjrodan@gmail.com>
2019-11-07 21:42:42 -0500

    env

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// require('dotenv').config();
+require('dotenv').config();
 const server = express();
 const port = process.env.PORT;
 server.use(express.json());
